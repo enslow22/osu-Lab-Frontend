cbb3bb0a1b9e0dbde2774f0901bf45a63c3aec0b
Ryan Enslow <enslow22@up.edu>
2025-02-28 22:38:14 -0600

    Fixed a .env bug

--- src/routes/leaderboards/+page.server.ts
+++ src/routes/leaderboards/+page.server.ts
@@ -1,9 +1,9 @@
-import { API_DOMAIN } from '$env/static/private';
+import { PUBLIC_API_DOMAIN } from '$env/static/private';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 
-    let leaderboardsDataFetchUrl = `${API_DOMAIN}/get_leaderboards`
+    let leaderboardsDataFetchUrl = `${PUBLIC_API_DOMAIN}/get_leaderboards`
     return {
         leaderboards: await fetch(leaderboardsDataFetchUrl).then((res) => res.json()),
     };
--- src/routes/leaderboards/[leaderboard_id]/+page.server.ts
+++ src/routes/leaderboards/[leaderboard_id]/+page.server.ts
@@ -1,9 +1,9 @@
-import { API_DOMAIN } from '$env/static/private';
+import { PUBLIC_API_DOMAIN } from '$env/static/private';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
 
-    let leaderboardsDataFetchUrl = `${API_DOMAIN}/get_leaderboard_info?leaderboard_id=${params.leaderboard_id}`
+    let leaderboardsDataFetchUrl = `${PUBLIC_API_DOMAIN}/get_leaderboard_info?leaderboard_id=${params.leaderboard_id}`
     return {
         leaderboardInfo: await fetch(leaderboardsDataFetchUrl).then((res) => res.json()),
     };
