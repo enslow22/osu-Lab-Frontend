89c538f21baaff2507d4a33b411442075f87b993
Ryan Enslow <enslow22@up.edu>
2025-03-06 10:47:31 -0600

    Fixed cookie auth req headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,7 +4,8 @@ import type { HandleFetch } from '@sveltejs/kit';
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
     
 	if (request.url.startsWith(`${PUBLIC_API_DOMAIN}`)) {
-		request.headers.set('session_token', event.request.headers.get('session_token'));
+		const sessionToken = event.request.headers.get('cookie') || '';
+		request.headers.set('session_token', sessionToken);
 	}
 
 	return await fetch(request);
